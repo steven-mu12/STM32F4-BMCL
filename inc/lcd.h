21709fee2b2a108d2e406e363a8b29bf2b272c42
Steven Mu <xsmu0922@gmail.com>
2024-04-15 01:47:38 -0400

    add 1602 setCursor + bug fix

--- inc/lcd.h
+++ inc/lcd.h
@@ -14,12 +14,12 @@ struct lcd {
 // ----- USER FUNCTIONS DECLARATIONS -----
 void initDisplay( struct lcd *lcd, uint16_t D4i, uint16_t D5i, uint16_t D6i, uint16_t D7i, uint16_t RSi, uint16_t Ei );
 void clearDisplay( struct lcd *lcd );
-void writeData( struct lcd *lcd, char *data, int length );
-
+void writeDisplay( struct lcd *lcd, char *data, int length );
+void setCursor( struct lcd *lcd, int row, int col );
 
 
 // ----- LOW LEVEL FUNCTIONS DECLARATIONS -----
-// note that the user should not use there directly
+// note that the user should not use these directly
 void _toggle_en( struct lcd *lcd );
 void _write_command( struct lcd *lcd, uint8_t command);
 void _write_fourbits( struct lcd *lcd, uint8_t command );
--- src/lcd.c
+++ src/lcd.c
@@ -1,6 +1,8 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
+#include <inttypes.h>
 #include "lcd.h"
 #include "gpio.h"
 #include "delay.h"
@@ -13,31 +15,28 @@
 
 void initDisplay( struct lcd *lcd, uint16_t D4i, uint16_t D5i, uint16_t D6i, uint16_t D7i, uint16_t RSi, uint16_t Ei ){
 
-	uint16_t *pins = &lcd->D7;
+	// assign pins to struct, and set them to output
+    lcd->D4 = D4i;
+    lcd->D5 = D5i;
+    lcd->D6 = D6i;
+    lcd->D7 = D7i;
+    lcd->RS = RSi;
+    lcd->E = Ei;
 
-	// --- GPIO initializations
-	gpioPinmode(D7i, GPIO_MODE_OUTPUT);
-	pins[0] = D7i;
-	gpioPinmode(D6i, GPIO_MODE_OUTPUT);
-	pins[1] = D6i;
-	gpioPinmode(D5i, GPIO_MODE_OUTPUT);
-	pins[2] = D5i;
-	gpioPinmode(D4i, GPIO_MODE_OUTPUT);
-	pins[3] = D4i;
-	gpioPinmode(RSi, GPIO_MODE_OUTPUT);
-	pins[4] = RSi;
-	gpioPinmode(Ei, GPIO_MODE_OUTPUT);
-	pins[5] = Ei;
+    gpioPinmode(D4i, GPIO_MODE_OUTPUT);
+    gpioPinmode(D5i, GPIO_MODE_OUTPUT);
+    gpioPinmode(D6i, GPIO_MODE_OUTPUT);
+    gpioPinmode(D7i, GPIO_MODE_OUTPUT);
+    gpioPinmode(RSi, GPIO_MODE_OUTPUT);
+    gpioPinmode(Ei, GPIO_MODE_OUTPUT);
 
 	// need to wait 50ms before any init commands
 	delay(50);
 
-	gpioDigitalWrite(lcd->E, 0); // Data mode
-	gpioDigitalWrite(lcd->RS, 0); // Data mode
-	delay(1);
+	gpioDigitalWrite(lcd->E, 0);
+	gpioDigitalWrite(lcd->RS, 0);
 
-
-	// still have no idea why this
+	// flash 0x03 3 times to force 4-bit mode
     _write_fourbits(lcd, 0x03);
     delay(8);
     _write_fourbits(lcd, 0x03);
@@ -48,43 +47,38 @@ void initDisplay( struct lcd *lcd, uint16_t D4i, uint16_t D5i, uint16_t D6i, uin
 
 
     // Now the LCD is in 4-bit mode, proceed with the rest of the initialization
-    _write_command(lcd, 0x28);  // Function Set: 4-bit mode, 2 lines, 5x8 font
-    _write_command(lcd, 0x0C);  // Display ON, Cursor OFF, Blink OFF
-    _write_command(lcd, 0x06);  // Entry Mode Set: Increment cursor, no display shift
-    _write_command(lcd, 0x01);  // Clear display
+    _write_command(lcd, 0x28);  // 4-bit mode, 2 lines, 5x8 font
+    _write_command(lcd, 0x0C);  // display ON, cursor OFF, blink OFF
+    _write_command(lcd, 0x06);  // cursor increments to the right
+    _write_command(lcd, 0x01);  // clear display
     delay(2);                   // clear needs about 1.53 ms
 }
 
 
-
-
 void clearDisplay( struct lcd *lcd ) {
-	gpioDigitalWrite(lcd->RS, 0); // Data mode
-	delay(1);
-
 	// clear LCD command
 	_write_command(lcd, 0b00000001);
-	delay(5);
-
-	// go home
-	_write_command(lcd, 0b10000000);
-	delay(5);
+	delay(2);
 }
 
 
+void setCursor( struct lcd *lcd, int row, int col ){
+	if (row == 0){
+		_write_command( lcd, ((col & 0x0F) | 0x80));
+	}
+	else if (row == 1){
+		_write_command( lcd, ((col & 0x0F) | 0xC0));
+	}
+}
 
 
-void writeData(struct lcd *lcd, char *data, int length) {
-    gpioDigitalWrite(lcd->RS, 1); // Data mode
-    delay(1);
-
+void writeDisplay(struct lcd *lcd, char *data, int length) {
+    gpioDigitalWrite(lcd->RS, 1); // Set to data mode
     for (int i = 0; i < length; i++) {
         _write_fourbits(lcd, data[i] >> 4); // High nibble
         _write_fourbits(lcd, data[i] & 0x0F); // Low nibble
     }
 }
-
-
 
 
 
@@ -97,22 +91,24 @@ void writeData(struct lcd *lcd, char *data, int length) {
 
 void _toggle_en( struct lcd *lcd ){
 	gpioDigitalWrite(lcd->E, 1);
-	delay(2);
+	delay(1);
 	gpioDigitalWrite(lcd->E, 0);
-	delay(5);
+	delay(1);
 }
 
 
 void _write_command(struct lcd *lcd, uint8_t command) {
-	_write_fourbits(lcd, command >> 4);
-	_write_fourbits(lcd, command & 0x0F);
+	gpioDigitalWrite(lcd->RS, 0); // Set to command mode
+	_write_fourbits(lcd, command >> 4); // High nibble
+	_write_fourbits(lcd, command & 0x0F); // Low nibble
 }
 
 
-
 void _write_fourbits(struct lcd *lcd, uint8_t command) {
-    for (int i = 0; i < 4; i++) {
-        gpioDigitalWrite((&lcd->D7)[i], (command >> i) & 1);
-    }
+    gpioDigitalWrite(lcd->D4, (command & 1) ? 1 : 0);
+    gpioDigitalWrite(lcd->D5, (command & 2) ? 1 : 0);
+    gpioDigitalWrite(lcd->D6, (command & 4) ? 1 : 0);
+    gpioDigitalWrite(lcd->D7, (command & 8) ? 1 : 0);
+
     _toggle_en(lcd);
 }
