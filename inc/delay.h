d5b4c0925adff160d84be007334f1e437679ecd6
Steven Mu <xsmu0922@gmail.com>
2024-04-18 14:34:09 -0400

    add us delay support

--- inc/delay.h
+++ inc/delay.h
@@ -3,8 +3,47 @@
 
 #include <stdint.h>
 
-void systick_init(uint32_t ticks);
+
+// TIM2 struct
+#define CPU_CLOCK_HZ   16000000
+#define PRESCALER      (CPU_CLOCK_HZ / 1000000) - 1  // prescaler
+
+struct tim2 {
+    volatile uint32_t CR1;
+    volatile uint32_t CR2;
+    volatile uint32_t SMCR;
+    volatile uint32_t DIER;
+    volatile uint32_t SR;
+    volatile uint32_t EGR;
+    volatile uint32_t CCMR1;
+    volatile uint32_t CCMR2;
+    volatile uint32_t CCER;
+    volatile uint32_t CNT;
+    volatile uint32_t PSC;
+    volatile uint32_t ARR;
+    volatile uint32_t RESERVED0;
+    volatile uint32_t CCR1;
+    volatile uint32_t CCR2;
+    volatile uint32_t CCR3;
+    volatile uint32_t CCR4;
+    volatile uint32_t RESERVED1;
+    volatile uint32_t DCR;
+    volatile uint32_t DMAR;
+    volatile uint32_t OR;
+};
+#define TIM2 ((struct tim2 *) 0x40000000)
+
+
+
+// ------ MS DELAY ------
+void systickInit(uint32_t ticks);
 void SysTick_Handler(void);
 void delay(unsigned ms);
+
+
+// ------ US DELAY ------
+void usDelayInit(void);
+void delayUs( uint32_t microseconds );
+
 
 #endif
--- src/delay.c
+++ src/delay.c
@@ -1,14 +1,28 @@
 #include "delay.h"
 #include "gpio.h"
 
+
+// ---------------------
+// ------ HANDLER ------
+// ---------------------
+volatile uint32_t ms_ticks;
+
 struct systick {
   volatile uint32_t CTRL, LOAD, VAL, CALIB;
 };
-#define SYSTICK ((struct systick *) 0xe000e010)
+#define SYSTICK ((struct systick *) 0xE000E010)
 
-volatile uint32_t s_ticks;
+void SysTick_Handler(void) {
+	ms_ticks++;
+}
 
-void systick_init(uint32_t ticks) {
+
+// ----------------------
+// ------ MS DELAY ------
+// ----------------------
+
+// -- tick = 16000 should be used
+void systickInit( uint32_t ticks ) {
 	if ((ticks - 1) > 0xffffff) return;  		// systick timer is 24 bit
 	SYSTICK->LOAD = ticks - 1;
 	SYSTICK->VAL = 0;
@@ -16,11 +30,32 @@ void systick_init(uint32_t ticks) {
 	RCC->APB2ENR |= BIT(14);                   	// enable SYSCFG
 }
 
-void SysTick_Handler(void) {
-	s_ticks++;
+void delay(unsigned ms) {
+	uint32_t until = ms_ticks + ms;      			// future "stoppage time"
+	while (ms_ticks < until) (void) 0;   			// polling
 }
 
-void delay(unsigned ms) {
-	uint32_t until = s_ticks + ms;      			// future "stoppage time"
-	while (s_ticks < until) (void) 0;   			// polling
+
+
+// ----------------------
+// ------ US DELAY ------
+// ----------------------
+
+void usDelayInit(void) {
+    RCC->APB1ENR |= 0x1;
+
+    // reset the timer
+    TIM2->CR1 = 0;
+    TIM2->CNT = 0;
+
+    // apply prescaler + start timer
+    TIM2->PSC = PRESCALER;
+    TIM2->ARR = 0xFFFFFFFF;
+    TIM2->EGR = 0x01;
+    TIM2->CR1 |= 0x01;
+}
+
+void delayUs( uint32_t microseconds ) {
+    uint32_t start_time = TIM2->CNT;
+    while ((TIM2->CNT - start_time) < microseconds);
 }
